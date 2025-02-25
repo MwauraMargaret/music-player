07fb93fc6348d7ce6acb149a6f0216aa84832e15
MwauraMargaret <mwauramargaret254@gmail.com>
2025-02-25 11:01:21 +0300

    playing via spotify and youtube

--- main.py
+++ main.py
@@ -1,21 +1,58 @@
 import tkinter as tk
-import fnmatch
-import os
+import yt_dlp
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import requests
 from pygame import mixer
+
+mixer.init()
 
 canvas = tk.Tk()
 canvas.title("Musiki Player")
 canvas.geometry("600x800")
 canvas.config(bg='black')
 
-rootpath= "C:\\Users\Waithera\Music\Muziki"
-pattern = "*mp3"
+def play_youtube():
+    url = url_entry.get()
+    if url:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}],
+            'outtmpl': 'temp_song.mp3',
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        mixer.music.load("temp_song.mp3")
+        mixer.music.play()
 
-listBox = tk.Listbox(canvas, fg = "Cyan", bg ="black", width = 100, font = ('DS-Digital', 14))
-listBox.pack(padx = 15, pady = 15)
+def play_spotify():
+    url = url_entry.get()
+    if "spotify" not in url:
+        return
+    
+    CLIENT_ID = ""
+    CLIENT_SECRET = ""
 
-for root, dirs, files in os.walk(rootpath):
-    for filename in fnmatch.filter(files,pattern):
-        listBox.insert('end', filename)  
+    sp = spotipy.Spotify(auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET))
+
+    track_id = url.split("/")[-1].split("?")[0]
+    track_info = sp.track(track_id)
+    preview_url = track_info.get('preview_url')
+
+    if preview_url:
+        response = requests.get(preview_url)
+        with open("spotify_preview.mp3", "wb") as f:
+            f.write(response.content)
+        mixer.music.load("spotify_preview.mp3")
+        mixer.music.play()
+
+url_entry = tk.Entry(canvas, width=50)
+url_entry.pack(pady=5)
+
+yt_button = tk.Button(canvas, text="Play YouTube", command=play_youtube, bg="red", fg="white")
+yt_button.pack(pady=5)
+
+spotify_button = tk.Button(canvas, text="Play Spotify", command=play_spotify, bg="green", fg="white")
+spotify_button.pack(pady=5)
 
 canvas.mainloop()
